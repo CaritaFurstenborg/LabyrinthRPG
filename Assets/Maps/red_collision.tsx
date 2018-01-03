71f9253c14ba6229343c194e5253c4e4c74027d6
Carita Furstenborg <carita.furstenborg@gmail.com>
2018-01-03 14:11:47 +0200

    Puolivalmis kartta

---
+++ Assets/Maps/red_collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Art/collision_box_red.png" width="32" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.625" y="0.625" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+</tileset>
